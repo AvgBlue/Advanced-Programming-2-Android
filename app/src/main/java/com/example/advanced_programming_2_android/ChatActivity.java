75a5835fa1c5630a739e114fbbcc8a9d999da8ef
BarAharon <bar.aharon98@gmail.com>
2023-06-18 15:18:30 +0300

    commit

--- app/src/main/java/com/example/advanced_programming_2_android/ChatActivity.java
+++ app/src/main/java/com/example/advanced_programming_2_android/ChatActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.advanced_programming_2_android.classes.Chat;
