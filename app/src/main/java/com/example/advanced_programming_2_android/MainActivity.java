d4eaad852fd38497d6d925dfeff235301ac05ca0
Netanel Berkovits <nello0b@gmail.com>
2023-06-21 21:07:39 +0300

    Config saves and settings room

--- app/src/main/java/com/example/advanced_programming_2_android/MainActivity.java
+++ app/src/main/java/com/example/advanced_programming_2_android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.advanced_programming_2_android;
 
+import static com.example.advanced_programming_2_android.settings.ThemeManager.applyTheme;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -11,11 +14,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.advanced_programming_2_android.api.UserAPI;
+import com.example.advanced_programming_2_android.database.AppDB;
+import com.example.advanced_programming_2_android.database.Settings;
+import com.example.advanced_programming_2_android.database.SettingsDao;
+import com.example.advanced_programming_2_android.viewModels.PreferencesViewModel;
+import com.example.advanced_programming_2_android.viewModels.PreferencesViewModelFactory;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnSignIn;
     private Button btnRegister;
-    private ImageView settings;
+    private ImageView settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
         btnSignIn = findViewById(R.id.sign_in_btn);
         btnRegister = findViewById(R.id.register_btn);
-        settings = findViewById(R.id.settings_action_bar);
+        settingsButton = findViewById(R.id.settings_action_bar);
+
+        PreferencesViewModelFactory factory = new PreferencesViewModelFactory(getApplicationContext());
+        PreferencesViewModel preferencesViewModel = new ViewModelProvider(this, factory).get(PreferencesViewModel.class);
+        applyTheme(preferencesViewModel.getThemeLiveData().getValue());
+
 
         btnSignIn.setOnClickListener(view -> {
             Intent intent = new Intent(this, LogInActivity.class);
@@ -36,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        settings.setOnClickListener(view -> {
+        settingsButton.setOnClickListener(view -> {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
         });
